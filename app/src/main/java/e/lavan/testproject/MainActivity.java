b2f0b83f6375beb941ecea31f653cb8fe331ff67
Lava8866 <lavanya2204@hotmail.com>
2019-08-10 14:11:40 +0800

    Added comment

--- app/src/main/java/e/lavan/testproject/MainActivity.java
+++ app/src/main/java/e/lavan/testproject/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Testing 101
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
